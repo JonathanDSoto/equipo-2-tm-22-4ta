5437bd3ac26e68d05ddc9dce564b5b1a89b9fef0
Xavier Juarez <xavier_9990@hotmail.com>
2022-10-13 18:45:06 -0600

    Se agrego controlador de categorias

---
+++ app/CathController.php
@@ -0,0 +1,193 @@
+<?php
+
+include_once  "config.php";
+
+#CRUD
+if(isset($_POST['action'])){
+    switch($_POST['action']){
+      case 'create':
+        
+        $name = strip_tags($_POST['name']);
+        $description = strip_tags($_POST['description']);
+        $slug = strip_tags($_POST['slug']);
+        $category_id = strip_tags($_POST['category_id']);
+
+        $cath = new CathController();
+
+        $cath->create($name, $description, $slug,$category_id); 
+
+      break;
+      case 'update':
+        $id = strip_tags($_POST['id']);
+        $name = strip_tags($_POST['name']);
+        $description = strip_tags($_POST['description']);
+        $slug = strip_tags($_POST['slug']);
+        $category_id = strip_tags($_POST['category_id']);
+
+        $cath = new CathController;
+
+        $cath->editCath($id,$name, $description, $slug,$category_id);
+
+      break;
+      case 'remove':
+        $id = strip_tags($_POST['id']);
+
+        $cath = new CathController;
+
+        $cath->remove($id);
+
+      break;
+    }
+}
+
+class CathController{
+
+    #Get de todas las categorias:
+    public static function getCath(){
+        
+        $token = $_SESSION['token'];
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://crud.jonathansoto.mx/api/categories',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer '.$token
+          ),
+        ));$response = curl_exec($curl); 
+        curl_close($curl);
+        $response = json_decode($response);
+        
+        if ( isset($response->code) && $response->code > 0) {
+            
+            return $response->data;
+        }else{
+        
+            return array();
+        }
+            }
+
+     #Crear Categorias:
+     public function create($name, $description, $slug,$category_id){
+
+        $token = $_SESSION['token'];    
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://crud.jonathansoto.mx/api/categories',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => array('name' => $name,'description' =>$description,'slug' => $slug,'category_id' =>$category_id),
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer '.$token
+        ),
+         ));
+        $response = curl_exec($curl);
+        curl_close($curl);
+
+        header('location: '.BASE_PATH.'view/index.php');
+        var_dump($response);
+      
+      }
+
+
+    #Editar tags:
+    public function editCath($id,$name, $description, $slug,$category_id)
+    {
+        $token = $_SESSION['token'];
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://crud.jonathansoto.mx/api/categories',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'PUT',
+        CURLOPT_POSTFIELDS => 'name=' . $name.'&slug='.$slug.'&description='.$description.'&id='.$id.'&category_id='.$category_id,
+        CURLOPT_HTTPHEADER => array(
+          'Authorization: Bearer '.$_SESSION['token']
+        ),
+      ));
+      $response = curl_exec($curl);
+  
+      curl_close($curl);
+      if (isset ($response->code) && $response->code > 0){
+        header('location: '.BASE_PATH.'view/index.php');
+      } else {
+        header('location: '.BASE_PATH.'view/index.php?error=false');
+      }
+    }
+
+
+    #Elminar categorias por ID:
+    public function remove($id){
+      
+        $token = $_SESSION['token'];
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://crud.jonathansoto.mx/api/categories/'.$id,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'DELETE',
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer ' . $_SESSION['token']
+        ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        $response = json_decode ($response);
+        if (isset ($response->code) && $response->code > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    } 
+
+    #Get categoria especifica:
+    public function getSSpecifCath($id){
+
+        $token = $_SESSION['token'];
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://crud.jonathansoto.mx/api/categories/'.$id,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer ' . $_SESSION['token']),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        echo $response;
+    }
+
+}
+?>
