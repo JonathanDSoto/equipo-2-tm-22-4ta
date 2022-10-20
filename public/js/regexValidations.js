c327a17bed15de98502c5c3c1821506d1903a861
Enrique León <len.geraldo@yahoo.com>
2022-10-19 20:59:27 -0600

    Módulo de Productos con Presentaciones y Orders

--- public/js/regexValidations.js
+++ public/js/regexValidations.js
@@ -14,6 +14,10 @@ function numbersLettersSpaces(e) {
     return /[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ ]/i.test(e.key);
 }
 
+function numbersLettersWithoutSpaces(e) {
+    return /[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ]/i.test(e.key);
+}
+
 function onlyLettersAndSpaces(e) {
     return /[a-zA-ZñÑáéíóúÁÉÍÓÚ ]/i.test(e.key);
 }
